b4b2f5c3494751662f09122f7bed2be343f7e7ac
Toshi Stewart <toshi.stewart@gmail.com>
2019-02-19 14:15:09 +0100

    Reconnected to API

--- app/js/_dev/_data/_data-ladder.js
+++ app/js/_dev/_data/_data-ladder.js
@@ -4,9 +4,9 @@
 // ====
 function dataLadder() { 
 
-    // $.getJSON('https://statsapi.foxsports.com.au/3.0/api/sports/afl/series/1/seasons/123/ladder.json?userkey=6B2F4717-A97C-49F6-8514-3600633439B9', function(json){
+    $.getJSON('https://statsapi.foxsports.com.au/3.0/api/sports/afl/series/1/seasons/123/ladder.json?userkey=6B2F4717-A97C-49F6-8514-3600633439B9', function(json){
 
-    $.getJSON('../data/dummy_data.json', function (json) {
+    // $.getJSON('../data/dummy_data.json', function (json) {
         var round = $('.c-ladder__round');
 
         // console.log(json);
