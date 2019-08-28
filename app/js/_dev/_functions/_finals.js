38c371ad4263be05e44fd623888cce8b4e167a31
Toshi Stewart <toshi.stewart@gmail.com>
2019-08-28 09:42:08 +0200

    Time for the FINALS!

--- app/js/_dev/_functions/_finals.js
+++ app/js/_dev/_functions/_finals.js
@@ -74,13 +74,13 @@ function finals(data) {
 
    // Qualifying / Elimiation Finals =========
    // First Qualifying Final
-   finalBuilder($('.js-finals-qf1'), data[0]);
+   finalBuilder($('.js-finals-qf1'), data[1]);
    // Second Qualifying Final
-   finalBuilder($('.js-finals-qf2'), data[1]);
+   finalBuilder($('.js-finals-qf2'), data[3]);
    // First Elimination Final
-   finalBuilder($('.js-finals-ef1'), data[2]);
+   finalBuilder($('.js-finals-ef1'), data[0]);
    // Second Elimination Final
-   finalBuilder($('.js-finals-ef2'), data[3]);
+   finalBuilder($('.js-finals-ef2'), data[2]);
 
 
    // Semi Finals ============
