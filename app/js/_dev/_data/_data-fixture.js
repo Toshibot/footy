28370a3d99d9eccedfd08561e82de5c7cc97fa1a
Toshi Stewart <mail@toshistewart.com>
2020-09-20 13:45:57 +0200

    Setup for 2020 finals

--- app/js/_dev/_data/_data-fixture.js
+++ app/js/_dev/_data/_data-fixture.js
@@ -2,16 +2,16 @@
 // Data - Fixture/Results
 
 function dataFixture(round) {
-    
+
     $.getJSON('https://statsapi.foxsports.com.au/3.0/api/sports/afl/series/1/seasons/124/fixturesandresults.json?userkey=6B2F4717-A97C-49F6-8514-3600633439B9', function (json) {
 
-    // Dummy Dev File
-    // $.getJSON('../data/data-fixture.json', function(json){
+        // Dummy Dev File
+        // $.getJSON('../data/data-fixture.json', function(json){
 
         // console.log(json);
         console.log('fixture loaded');
         console.log(json);
-        
+
         var today = new Date;
         var testDate = new Date('2018-04-24');
         var currentRound = [];
@@ -20,10 +20,10 @@ function dataFixture(round) {
         // var currentRoundNo = 23;
 
         $('.js-fixture-round').text(currentRoundNo);
-        
+
         for (i = 0; i < json.length; i++) {
             const element = json[i];
-            
+
             if (element.round.number == currentRoundNo) {
                 currentRound.push(element);
             }
@@ -38,20 +38,20 @@ function dataFixture(round) {
 
             fixtureItem(element);
         }
-        
+
         // scroll();
 
 
-    
-        // for (i = 0; i < json.length; i++) {
-        //     const e = json[i];
-            
-        //     if (e.is_final == true) {
-        //         finalsData.push(e);
-        //     }
-        // }
 
-        // finals(finalsData);
-        
+        for (i = 0; i < json.length; i++) {
+            const e = json[i];
+
+            if (e.is_final == true) {
+                finalsData.push(e);
+            }
+        }
+
+        finals(finalsData);
+
     });
 }
