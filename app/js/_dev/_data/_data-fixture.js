5f01c79e6dc2962e3956cb2827819326c35909e8
Toshi Stewart <mail@toshistewart.com>
2021-03-19 09:42:33 +0100

    Updated to 2021 Season

--- app/js/_dev/_data/_data-fixture.js
+++ app/js/_dev/_data/_data-fixture.js
@@ -3,7 +3,7 @@
 
 function dataFixture(round) {
 
-    $.getJSON('https://statsapi.foxsports.com.au/3.0/api/sports/afl/series/1/seasons/124/fixturesandresults.json?userkey=6B2F4717-A97C-49F6-8514-3600633439B9', function (json) {
+    $.getJSON('https://statsapi.foxsports.com.au/3.0/api/sports/afl/series/1/seasons/125/fixturesandresults.json?userkey=6B2F4717-A97C-49F6-8514-3600633439B9', function (json) {
 
         // Dummy Dev File
         // $.getJSON('../data/data-fixture.json', function(json){
