4ecc5cc35d663ae730b05661ab0fe13f6b707679
Toshi Stewart <>
2021-05-31 13:55:58 +0200

    Removed redundant code

--- app/js/main.js
+++ app/js/main.js
@@ -4,223 +4,41 @@
 
 // Core Functions 
 data();
-var data_club = {
-    ADE: {
-        name: 'Adelaide',
-        logo: 'img/teams/crows.png',
-        kit: {
-            home: 'img/teams/crows-home.png',
-            away: 'img/teams/crows-away.png',
-            clash: {
-                kit: 'img/teams/crows-home.png',
-                teams: ['HAW']
-            }
-        }
-    },
-    BRI: {
-        name: 'Brisbane',
-        logo: 'img/teams/lions.png',
-        kit: {
-            home: 'img/teams/lions-home.png',
-            away: 'img/teams/lions-away.png',
-            clash: {
-                kit: 'img/teams/lions-clash.png',
-                teams: ['SYD','GWS','GCS','WCE','ADE','FRE','POR', 'MEL']
-            }
-        }
-    },
-    CAR: {
-        name: 'Carlton',
-        logo: 'img/teams/blues.png',
-        kit: {
-            home: 'img/teams/blues-home.png',
-            away: 'img/teams/blues-away.png',
-            clash: {
-                kit: 'img/teams/blues-home.png',
-                teams: ['GEE', 'SYD']
-            }
-        }
-    },
-    COL: {
-        name: 'Collingwood',
-        logo: 'img/teams/magpies.png',
-        kit: {
-            home: 'img/teams/magpies-home.png',
-            away: 'img/teams/magpies-away.png',
-            clash: {
-                kit: 'img/teams/magpies-home.png',
-                teams: ['SYD', 'GEE']
-            }
-        }
-    },
-    ESS: {
-        name: 'Essendon',
-        logo: 'img/teams/bombers.png',
-        kit: {
-            home: 'img/teams/bombers-home.png',
-            away: 'img/teams/bombers-home.png',
-            clash: {
-                kit: 'img/teams/bombers-clash.png',
-                teams: ['MEL', 'COL', 'CAR', 'POR', 'STK', 'RIC']
-            }
-        }
-    },
-    FRE: {
-        name: 'Fremantle',
-        logo: 'img/teams/dockers.png',
-        kit: {
-            home: 'img/teams/dockers-home.png',
-            away: 'img/teams/dockers-away.png',
-            clash: {
-                kit: 'img/teams/dockers-home.png',
-                teams: ['SYD', 'GEE']
-            }
-        }
-    },
-    GEE: {
-        name: 'Geelong',
-        logo: 'img/teams/lions.png',
-        kit: {
-            home: 'img/teams/cats-home.png',
-            away: 'img/teams/cats-home.png',
-            clash: {
-                kit: 'img/teams/cats-clash.png',
-                teams: []
-            }
-        }
-    },
-    GCS: {
-        name: 'Gold Coast',
-        logo: 'img/teams/suns.png',
-        kit: {
-            home: 'img/teams/suns-home.png',
-            away: 'img/teams/suns-away.png',
-            clash: {
-                kit: 'img/teams/suns-home.png',
-                teams: ['WBD', 'CAR', 'FRE', 'NM', 'WCE']
-            }
-        }
-    },
-    GWS: {
-        name: 'Gr. W. Sydney',
-        logo: 'img/teams/gws.png',
-        kit: {
-            home: 'img/teams/gws-home.png',
-            away: 'img/teams/gws-away.png',
-            clash: {
-                kit: 'img/teams/gws-home.png',
-                teams: ['SYD', 'GEE']
-            }
-        }
-    },
-    HAW: {
-        name: 'Hawthorn',
-        logo: 'img/teams/hawks.png',
-        kit: {
-            home: 'img/teams/hawks-home.png',
-            away: 'img/teams/hawks-away.png',
-            clash: {
-                kit: 'img/teams/hawks-home.png',
-                teams: []
-            }
-        }
-    },
-    MEL: {
-        name: 'Melbourne',
-        logo: 'img/teams/demons.png',
-        kit: {
-            home: 'img/teams/demons-home.png',
-            away: 'img/teams/demons-away.png',
-            clash: {
-                kit: 'img/teams/demons-home.png',
-                teams: []
-            }
-        }
-    },
-    NM: {
-        name: 'N. Melbourne',
-        logo: 'img/teams/kangaroos.png',
-        kit: {
-            home: 'img/teams/kangaroos-home.png',
-            away: 'img/teams/kangaroos-clash.png',
-            clash: {
-                kit: 'img/teams/kangaroos-home.png',
-                teams: ['COL']
-            }
-        }
-    },
-    POR: {
-        name: 'Port Adelaide',
-        logo: 'img/teams/power.png',
-        kit: {
-            home: 'img/teams/power-home.png',
-            away: 'img/teams/power-away.png',
-            clash: {
-                kit: 'img/teams/power-home.png',
-                teams: ['SYD', 'GEE']
-            }
-        }
-    },
-    RIC: {
-        name: 'Richmond',
-        logo: 'img/teams/tigers.png',
-        kit: {
-            home: 'img/teams/tigers-home.png',
-            away: 'img/teams/tigers-away.png',
-            clash: {
-                kit: 'img/teams/tigers-home.png',
-                teams: ['HAW']
-            }
-        }
-    },
-    STK: {
-        name: 'St. Kilda',
-        logo: 'img/teams/saints.png',
-        kit: {
-            home: 'img/teams/saints-home.png',
-            away: 'img/teams/saints-away.png',
-            clash: {
-                kit: 'img/teams/saints-home.png',
-                teams: ['SYD', 'GEE']
-            }
-        }
-    },
-    SYD: {
-        name: 'Sydney',
-        logo: 'img/teams/swans.png',
-        kit: {
-            home: 'img/teams/swans-home.png',
-            away: 'img/teams/swans-home.png',
-            clash: {
-                kit: 'img/teams/swans-home.png',
-                teams: []
-            }
-        }
-    },
-    WBD: {
-        name: 'W. Bulldogs',
-        logo: 'img/teams/dogs.png',
-        kit: {
-            home: 'img/teams/dogs-home.png',
-            away: 'img/teams/dogs-away.png',
-            clash: {
-                kit: 'img/teams/dogs-home.png',
-                teams: ['SYD','GEE']
-            }
-        }
-    },
-    WCE: {
-        name: 'West Coast',
-        logo: 'img/teams/eagles.png',
-        kit: {
-            home: 'img/teams/eagles-home.png',
-            away: 'img/teams/eagles-away.png',
-            clash: {
-                kit: 'img/teams/eagles-home.png',
-                teams: ['HAW']
-            }
-        }
-    }
+function scroll() {
+   $(window).on('scroll', function(){
+      if ($(this).scrollTop() >= $('.c-fixture__round').offset().top - 500){
+         $('.js-scrollto-fixture').addClass('is-hidden');
+      } else {
+         $('.js-scrollto-fixture').removeClass('is-hidden');
+      }
+   });
+
+   $('.js-scrollto-fixture').on('click', function(){
+      $('html, body').animate(
+         {
+            scrollTop: $('.js-game-pregame').offset().top - 36,
+         }, {
+            duration: 400,
+            specialEasing: 'easeInOut'
+         }
+      )
+   });
+
+   $('.c-fixture__game').each(function(){
+      if ($(this).hasClass('c-fixture__game--in-progress')){
+         $('.js-scrollto-fixture').addClass('has-live');
+         $('.js-scrollto-fixture').on('click', function(){
+            $('html, body').animate(
+               {
+                  scrollTop: $('.c-fixture__game--in-progress').offset().top - 36,
+               }, {
+                  duration: 400,
+                  specialEasing: 'easeInOut'
+               }
+            )
+         });
+      }
+   });
 }
 
 // Data - Fixture/Results
@@ -309,111 +127,6 @@ function data() {
         dataLadder(clubs);
         dataFixture(clubs);
     });
-}
-// ==========================================================================
-// Fixture - Functions
-// ==========================================================================
-
-
-// 
-// Ladder
-// ======
-
-var ladder = function(){
-
-    // GWS
-    var giants = $('.c-ladder__team:contains("Greater Western Sydney")'); 
-    giants.children('span').text('GWS');
-    giants.children('img').attr('src', 'img/teams/gws.svg');
-
-    var hawks = $('.c-ladder__team:contains("Hawthorn")');
-    hawks.children('img').attr('src', 'img/teams/hawks.svg');
-
-    var suns = $('.c-ladder__team:contains("Gold Coast")');
-    suns.children('img').attr('src', 'img/teams/suns.svg');
-
-    var swans = $('.c-ladder__team:contains("Sydney")');
-    swans.children('img').attr('src', 'img/teams/swans.svg');
-
-    var saints = $('.c-ladder__team:contains("St Kilda")');
-    saints.children('img').attr('src', 'img/teams/saints.svg');
-
-    var tigers = $('.c-ladder__team:contains("Richmond")');
-    tigers.children('img').attr('src', 'img/teams/tigers.svg');
-
-    var bombers = $('.c-ladder__team:contains("Essendon")');
-    bombers.children('img').attr('src', 'img/teams/bombers.svg');
-
-    var cats = $('.c-ladder__team:contains("Geelong")');
-    cats.children('img').attr('src', 'img/teams/cats.svg');
-
-    var demons = $('.c-ladder__team:contains("Melbourne")');
-    demons.children('img').attr('src', 'img/teams/demons.svg');
-
-    var crows = $('.c-ladder__team:contains("Adelaide")');
-    crows.children('img').attr('src', 'img/teams/crows.svg');
-
-    var blues = $('.c-ladder__team:contains("Carlton")');
-    blues.children('img').attr('src', 'img/teams/blues.svg');
-    
-    // Port Adelaide
-    var port = $('.c-ladder__team:contains("Port Adelaide")');
-    port.children('img').attr('src', 'img/teams/port.svg');
-
-    var lions = $('.c-ladder__team:contains("Brisbane")');
-    lions.children('img').attr('src', 'img/teams/lions.svg');
-
-    var eagles = $('.c-ladder__team:contains("West Coast")');
-    eagles.children('img').attr('src', 'img/teams/eagles.svg');
-
-    var kangaroos = $('.c-ladder__team:contains("North Melbourne")');
-    kangaroos.children('img').attr('src', 'img/teams/kangaroos.svg');
-
-    var magpies = $('.c-ladder__team:contains("Collingwood")');
-    magpies.children('img').attr('src', 'img/teams/magpies.svg');
-
-    var dockers = $('.c-ladder__team:contains("Fremantle")');
-    dockers.children('img').attr('src', 'img/teams/dockers.svg');
-
-    var dogs = $('.c-ladder__team:contains("Bulldogs")');
-    dogs.children('img').attr('src', 'img/teams/dogs.svg');
-
-}
-function scroll() {
-   $(window).on('scroll', function(){
-      if ($(this).scrollTop() >= $('.c-fixture__round').offset().top - 500){
-         $('.js-scrollto-fixture').addClass('is-hidden');
-      } else {
-         $('.js-scrollto-fixture').removeClass('is-hidden');
-      }
-   });
-
-   $('.js-scrollto-fixture').on('click', function(){
-      $('html, body').animate(
-         {
-            scrollTop: $('.js-game-pregame').offset().top - 36,
-         }, {
-            duration: 400,
-            specialEasing: 'easeInOut'
-         }
-      )
-   });
-
-   $('.c-fixture__game').each(function(){
-      if ($(this).hasClass('c-fixture__game--in-progress')){
-         $('.js-scrollto-fixture').addClass('has-live');
-         $('.js-scrollto-fixture').on('click', function(){
-            $('html, body').animate(
-               {
-                  scrollTop: $('.c-fixture__game--in-progress').offset().top - 36,
-               }, {
-                  duration: 400,
-                  specialEasing: 'easeInOut'
-               }
-            )
-         });
-      }
-   });
 }
 
 function dateTime(d) {
@@ -772,15 +485,3 @@ $(window).on('resize', function() {
     // clearStyles($('.o-vert-center'), $('.o-vert-center__object'));
     // vertCenter($('.o-vert-center'), '.o-vert-center__object');
 });
-
-
-//
-// UI - Buttons
-// ==========================================================================
-
-// Variables
-// var gitButton = document.getElementById('js-button-github');
-
-// gitButton.addEventListener('click', function(){
-//     window.open('https://github.com/Toshibot/webapp-boilerplate', '_blank');
-// });
