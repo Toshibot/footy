c3ee5a7a367bbb2d4dee165c99e7be606fe39a8e
Toshi <toshi.stewart@gmail.com>
2026-01-21 11:49:55 +1100

    Updated for output

--- app/js/_dev/_data/_data-fixture.js
+++ app/js/_dev/_data/_data-fixture.js
@@ -3,7 +3,7 @@
 
 function dataFixture(clubs) {
 
-    $.getJSON('https://statsapi.foxsports.com.au/3.0/api/sports/afl/series/1/seasons/126/fixturesandresults.json?userkey=6B2F4717-A97C-49F6-8514-3600633439B9', function (json) {
+    $.getJSON('https://statsapi.foxsports.com.au/3.0/api/sports/afl/series/1/seasons/131/fixturesandresults.json?userkey=6B2F4717-A97C-49F6-8514-3600633439B9', function (json) {
 
         // Dummy Dev File
         // $.getJSON('../data/data-fixture.json', function(json){
--- app/js/main.js
+++ app/js/main.js
@@ -45,7 +45,7 @@ function scroll() {
 
 function dataFixture(clubs) {
 
-    $.getJSON('https://statsapi.foxsports.com.au/3.0/api/sports/afl/series/1/seasons/126/fixturesandresults.json?userkey=6B2F4717-A97C-49F6-8514-3600633439B9', function (json) {
+    $.getJSON('https://statsapi.foxsports.com.au/3.0/api/sports/afl/series/1/seasons/131/fixturesandresults.json?userkey=6B2F4717-A97C-49F6-8514-3600633439B9', function (json) {
 
         // Dummy Dev File
         // $.getJSON('../data/data-fixture.json', function(json){
@@ -108,7 +108,7 @@ function dataFixture(clubs) {
 
 function dataLadder(clubs) {
 
-    $.getJSON('https://statsapi.foxsports.com.au/3.0/api/sports/afl/series/1/seasons/126/ladder.json?userkey=6B2F4717-A97C-49F6-8514-3600633439B9', function (json) {
+    $.getJSON('https://statsapi.foxsports.com.au/3.0/api/sports/afl/series/1/seasons/130/ladder.json?userkey=6B2F4717-A97C-49F6-8514-3600633439B9', function (json) {
 
         // $.getJSON('../data/dummy_data.json', function (json) {
         var round = $('.c-ladder__round');
